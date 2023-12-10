4ef178c19a535485b953e6788e84d19aaff6d2be
Vitto633 <vitor.santos633@etec.sp.gov.br>
2023-12-10 20:27:53 -0300

    fix:alteracoes na pagina principal

--- projeto/assets/js/script.js
+++ projeto/assets/js/script.js
@@ -1,4 +1,9 @@
 
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
+
 function login(){
     let login = document.getElementById("login_email").value
     let senha = document.getElementById("login_senha").value
@@ -34,3 +39,33 @@ function clickmenu(){
         itens.style.display = 'block'
     }
 }
+
+
+controls.forEach((control) => {
+  control.addEventListener("click", (e) => {
+    isLeft = e.target.classList.contains("arrow-left");
+
+    if (isLeft) {
+      currentItem -= 1;
+    } else {
+      currentItem += 1;
+    }
+
+    if (currentItem >= maxItems) {
+      currentItem = 0;
+    }
+
+    if (currentItem < 0) {
+      currentItem = maxItems - 1;
+    }
+
+    items.forEach((item) => item.classList.remove("current-item"));
+
+    // items[currentItem].scrollIntoView({
+    //   behavior: "smooth",
+    //   inline: "center"
+    // });
+
+    items[currentItem].classList.add("current-item");
+  });
+}); 
